45935a52a989ab4ef018d7e2511975c7d699d762
Alfrinatiaraandini <andinitiara020706@gmail.com>
2025-11-18 12:25:17 +0700

    TIARA
    coba.cpp
    coba.java
    coba.php
    coba.py

---
+++ latihan cpp/coba.php/c1.php
@@ -0,0 +1,11 @@
+<?php
+$nama ="TIARA";
+$umur = 19;
+$tinggi = 147;
+$aktif = true;
+
+echo "Nama : $nama <br>";
+echo "Umur : $umur <br>";   
+echo "Tinggi : $tinggi <br>";
+echo "Aktif : $aktif <br>";
+?>
---
+++ latihan cpp/coba.py/c1.py
@@ -0,0 +1,9 @@
+nama = "TIARA";
+umur = 19
+tinggi = 147
+aktif = TRUE
+
+print("nama:", nama)
+print("umur:", umur)
+print("tinggi:", tinggi)
+print("aktif:", aktif)
